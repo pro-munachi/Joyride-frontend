752b477eedf9adb93c62f52fb4525ba125f4e916
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-09-10 20:24:46 +0100

    dependency

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import PrimarySearchAppBar from './components/navbar'
+import axios from 'axios'
 
 function App() {
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://kidsio.herokuapp.com/users')
+      .then((res) => {
+        setUser(res.data)
+        console.log(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <PrimarySearchAppBar />
+      {user.map((use) => (
+        <div key={use._id}>
+          <h1>{use.displayName}</h1>
+          <img src={use.profilePic} />
+          <p>{use.roles}</p>
+        </div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
